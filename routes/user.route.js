911992a0fab42bce375a9ae96d14f0d05c721207
malek-bachta <75339154+malek-bachta@users.noreply.github.com>
2023-01-03 09:26:29 +0100

    forget

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,5 +7,10 @@ const RequireLogin = require("../middleware/RequireLogin");
 router.get("/all", RequireLogin, userController.index);
 router.post("/login", userController.signin);
 router.post("/register", userController.signup);
+//
+router.post("/SendCodeForgot", userController.SendCodeForgot); //  Send FogetCode
+router.post("/VerifCodeForgot", userController.VerifCodeForgot); //  Verif FogetCode
+router.post("/ChangePasswordForgot", userController.ChangePasswordForgot); //  Change Password Forget
+//
 
 module.exports = router;
